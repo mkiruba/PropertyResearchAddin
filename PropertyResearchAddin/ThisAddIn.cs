6d865a8e888e75f1f6c47190c997e5a4ee8bf43e
mkiru <Kirubanandam.Marimuthu@equilend.com>
2018-03-07 16:54:33 +0000

    Initial checkin

--- PropertyResearchAddin/ThisAddIn.cs
+++ PropertyResearchAddin/ThisAddIn.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using Microsoft.Office.Tools;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
@@ -11,8 +12,13 @@ namespace PropertyResearchAddin
 {
     public partial class ThisAddIn
     {
+        private PropertyResearchControl propertyResearchControl;
+        private CustomTaskPane propertyResearchCtp;
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            propertyResearchControl = new PropertyResearchControl();
+            propertyResearchCtp = this.CustomTaskPanes.Add(propertyResearchControl, "Property Research Pane");
+            propertyResearchCtp.Visible = true;
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
